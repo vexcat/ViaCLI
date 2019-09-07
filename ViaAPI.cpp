ffe5420d3d8dbaf381f724eaa24c385bf9c1b47d
Gabriel Pulido <autodeskgato@gmail.com>
2019-09-06 17:06:27 -0700

    Check response code to make sure API responded well

--- ViaAPI.cpp
+++ ViaAPI.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <sstream>
 #include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+#include <curlpp/Infos.hpp>
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
@@ -11,6 +14,7 @@ extern std::atomic<bool> shouldICrash;
 extern std::condition_variable threadNotifier;
 
 const std::string api = "https://data.vexvia.dwabtech.com/api/v3/";
+//const std::string api = "https://google.com/api/v3/";
 
 //CurlPP uses a global object to init/destruct itself.
 #pragma clang diagnostic push
@@ -23,9 +27,15 @@ std::string download(const std::string& url) {
     std::atomic_bool doneVar;
     doneVar.store(false);
     std::ostringstream os;
+    bool wasSuccessful = true;
     std::thread dlThread([&]() {
+        curlpp::Easy request;
         std::cerr << url << std::endl;
-        os << curlpp::options::Url(std::string(url));
+        request.setOpt<curlpp::options::Url>(url);
+        request.setOpt<curlpp::options::WriteStream>(&os);
+        request.perform();
+        auto code = curlpp::infos::ResponseCode::get(request);
+        wasSuccessful = code == 200 || code == 204;
         doneVar.store(true);
         threadNotifier.notify_all();
     });
@@ -38,6 +48,9 @@ std::string download(const std::string& url) {
     }
     lk.unlock();
     dlThread.join();
+    if(!wasSuccessful) {
+        throw std::runtime_error("API did not send response code of 200 or 204.\n");
+    }
     return os.str();
 }
 
